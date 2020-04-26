fa61ff6789f6c6b5dc5df60646c00f3509ae4904
Aleksandr <aleksandrmihnovets@gmail.com>
2020-04-26 17:19:37 +0300

    write pretty recursive function

--- getPrimePartitions.js
+++ getPrimePartitions.js
@@ -5,16 +5,15 @@ function getPrimes(n) {
     return primes;
 }
 
-function getPrimePartitions(n, k = 2, minPrime = 2, primes = getPrimes(n-2*k+3)) {
-    let partitions = [];
-    for (let i = primes.indexOf(minPrime); i < primes.length; i++) {
-        const prime = primes[i];
-        if (prime > n / k) return partitions;
+function getPrimeSums(n, k = 2, min = 2, primes = getPrimes(n + 1)) {
+    let sums = [];
+    for (const prime of primes) {
+        if (prime > n / k) return sums;
         else if (k === 1 && prime === n) return [[prime]];
-        else if (k > 1) {
-            const restPartitions = getPrimePartitionsRec(n - prime, k - 1, prime, primes);
-            partitions = [...partitions, ...restPartitions.map(partition => [prime, ...partition])];
+        else if (k > 1 && prime >= min) {
+            const restSums = getPrimeSums(n - prime, k - 1, prime, primes);
+            sums = sums.concat(restSums.map(sum => [prime, ...sum]));
         }
     }
-    return partitions;
+    return sums;
 }
