f8042c4c07233da8a13d43a2fedae894dbd1465f
Aleksandr <aleksandrmihnovets@gmail.com>
2020-04-28 23:45:01 +0300

    refactoring

--- getPrimeSums.js
+++ getPrimeSums.js
@@ -6,7 +6,7 @@ const getPrimeSums = (n, k = 2, primes = eratosthenes(n + 1)) => {
     while (term >= 0) {
         if (sum === n) sums.push(indices.map(i => primes[i]))
         else if (sum < n) term = k - 1;
-        if (sum >= n || indices[term] + 1 === primes.length) term -= 1;
+        if (sum >= n || indices[term] === primes.length - 1) term -= 1;
         for (let i = k - 1; i >= term; i--) {
             sum += primes[indices[term] + 1] - primes[indices[i]];
             indices[i] = indices[term] + 1;
